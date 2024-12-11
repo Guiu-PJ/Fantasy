cf21ecdf9b1af5d7bab65c7120c2029728008910
Guiu <guiup01@gmail.com>
2024-12-11 20:36:49 +0100

    aaa

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const btnSignUp = document.getElementById('btnSignUp');
 const btnSignIn = document.getElementById('btnSignIn');
 
+const BASE_URL = 'https://fantasybackend-yok6.onrender.com';
+
+
+fetch(`${BASE_URL}/api/saludo`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.mensaje); // Muestra "Hola desde el backend!"
+    })
+    .catch(error => console.error('Error:', error));
 
 btnSignUp.addEventListener('click', () => {
     window.location.href = 'Views/signUp.html'; 
