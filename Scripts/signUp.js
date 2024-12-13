1a159225c9d7650ca858b29021e2fa925e06c15c
Guiu <guiup01@gmail.com>
2024-12-13 20:11:55 +0100

    aaaa

--- Scripts/signUp.js
+++ Scripts/signUp.js
@@ -2,6 +2,7 @@ import User from "../Models/User.js";
 
 const form = document.getElementById('signUpForm');
 
+const loadingIndicator = document.getElementById('loading');
 
 form.addEventListener('submit', async (event) => {
     // Prevenir el envío por defecto (para que no recargue la página)
@@ -10,12 +11,13 @@ form.addEventListener('submit', async (event) => {
     // Capturar los datos del formulario
     const nombre = document.getElementById('nombre').value;
     const password = document.getElementById('password').value;
-
+    loadingIndicator.style.display = 'block';
     const newUser = new User(nombre, password, 'user');
 
     const exists = await newUser.userExists();
     if (!exists) {
         await newUser.saveUser();
+        loadingIndicator.style.display = 'none';
     } else {
         alert("Ja existeix un usuari amb aquest nom");
     }
